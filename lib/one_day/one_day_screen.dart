85cb00120e0f1179b8e59b2db7eb1aece279cf06
zhaowenchao <13253536897@163.com>
2020-09-01 23:05:42 +0800

    首页修改

--- lib/one_day/one_day_screen.dart
+++ lib/one_day/one_day_screen.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:timefly/app_theme.dart';
-import 'package:timefly/blocs/habit/habit_bloc.dart';
-import 'package:timefly/blocs/habit/habit_event.dart';
-import 'package:timefly/models/habit.dart';
-import 'package:timefly/widget/habits_list_view.dart';
-import 'package:timefly/widget/title_view.dart';
+import 'package:timefly/db/database_provider.dart';
+import 'package:timefly/utils/hex_color.dart';
 
 class OneDayScreen extends StatefulWidget {
   @override
@@ -16,49 +12,8 @@ class OneDayScreen extends StatefulWidget {
 
 class _OneDayScreenState extends State<OneDayScreen>
     with TickerProviderStateMixin {
-  AnimationController animationController;
-
-  //顶部动画
-  Animation<double> topBarAnimation;
-
-  List<Widget> listViews = <Widget>[];
-
-  final ScrollController scrollController = ScrollController();
-
-  //不透明度
-  double topBarOpacity = 0.0;
-
   @override
   void initState() {
-    animationController = AnimationController(
-        duration: const Duration(milliseconds: 600), vsync: this);
-    topBarAnimation = Tween<double>(begin: 0, end: 1).animate(CurvedAnimation(
-        parent: animationController,
-        curve: Interval(0, 0.5, curve: Curves.fastOutSlowIn)));
-    initListViews();
-    scrollController.addListener(() {
-      if (scrollController.offset >= 24) {
-        if (topBarOpacity != 1.0) {
-          setState(() {
-            topBarOpacity = 1.0;
-          });
-        }
-      } else if (scrollController.offset <= 24 &&
-          scrollController.offset >= 0) {
-        if (topBarOpacity != scrollController.offset / 24) {
-          setState(() {
-            topBarOpacity = scrollController.offset / 24;
-          });
-        }
-      } else if (scrollController.offset <= 0) {
-        if (topBarOpacity != 0.0) {
-          setState(() {
-            topBarOpacity = 0.0;
-          });
-        }
-      }
-    });
-    print('init state');
     super.initState();
   }
 
@@ -66,164 +21,115 @@ class _OneDayScreenState extends State<OneDayScreen>
   Widget build(BuildContext context) {
     return Container(
       color: AppTheme.background,
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        body: Stack(
-          children: <Widget>[
-            getMainListViewUI(),
-            getAppBarUI(),
-            SizedBox(
-              height: MediaQuery.of(context).padding.bottom,
-            )
-          ],
+      child: FutureBuilder(
+        future: getListData(),
+        builder: (context, snapshot) {
+          if (snapshot.data == null) {
+            return SizedBox();
+          }
+          List<ListData> listData = snapshot.data;
+          return ListView.builder(
+              itemCount: listData.length,
+              itemBuilder: (context, index) {
+                ListData data = listData[index];
+                Widget widget;
+                switch (data.type) {
+                  case ListData.typeHeader:
+                    widget = getHeaderView();
+                    break;
+                  case ListData.typeTip:
+                    widget = getTipsView();
+                    break;
+                  case ListData.typeTitle:
+                    widget = Text('title');
+                    break;
+                  case ListData.typeHabit:
+                    widget = Text('habit');
+                    break;
+                }
+                return widget;
+              });
+        },
+      ),
+    );
+  }
+
+  ///获取列表所有数据
+  Future<List<ListData>> getListData() async {
+    List<ListData> datas = [];
+    datas.add(ListData(type: ListData.typeHeader, value: null));
+    var habits = await DatabaseProvider.db.getHabits();
+    if (habits.length > 0) {
+      datas.add(ListData(type: ListData.typeTip, value: habits.length));
+      for (var habit in habits) {
+        datas.add(ListData(type: ListData.typeHabit, value: habit));
+      }
+    } else {
+      datas.add(ListData(type: ListData.typeTip, value: null));
+    }
+    return datas;
+  }
+
+  Widget getHeaderView() {
+    return Padding(
+      padding: EdgeInsets.only(left: 20, top: 40, bottom: 30),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Hello,Good Morning',
+            style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            'You have 7 habits last !!',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget getTipsView() {
+    return Padding(
+      padding: EdgeInsets.only(left: 50),
+      child: Container(
+        alignment: Alignment.center,
+        height: 100,
+        decoration: BoxDecoration(
+            boxShadow: <BoxShadow>[
+              BoxShadow(
+                  color: HexColor('#738AE6').withOpacity(0.6),
+                  offset: const Offset(1.1, 4.0),
+                  blurRadius: 8.0),
+            ],
+            gradient: LinearGradient(
+              colors: <HexColor>[
+                HexColor('#5C5EDD'),
+                HexColor('#738AE6'),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20), bottomLeft: Radius.circular(20))),
+        child: Text(
+          'You can add a habit Here!!!',
+          style: TextStyle(
+              color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20),
         ),
       ),
     );
   }
+}
 
-  ///初始化页面内容
-  ///note view
-  ///1天分类view
-  ///2天分类view
-  ///
-  void initListViews() async {
-    int count = 2;
-    /* int period1Size = await DatabaseProvider.db.getPeriodHabitsSize(1);
-    int period2Size = await DatabaseProvider.db.getPeriodHabitsSize(2);
-    if (period1Size > 0) {
-      count += 2;
-    }
-    if (period2Size > 0) {
-      count += 2;
-    }*/
+class ListData {
+  static const int typeHeader = 0;
+  static const int typeTip = 1;
+  static const int typeTitle = 2;
+  static const int typeHabit = 3;
 
-    listViews.add(
-      TitleView(
-        titleTxt: 'Meals today',
-        subTxt: 'Customize',
-        animation: Tween<double>(begin: 0.0, end: 1.0).animate(CurvedAnimation(
-            parent: animationController,
-            curve:
-                Interval((1 / count) * 0, 1.0, curve: Curves.fastOutSlowIn))),
-        animationController: animationController,
-      ),
-    );
-    listViews.add(
-      HabitsListView(
-        mainScreenAnimation: Tween<double>(begin: 0.0, end: 1.0).animate(
-            CurvedAnimation(
-                parent: animationController,
-                curve: Interval((1 / count) * 1, 1.0,
-                    curve: Curves.fastOutSlowIn))),
-        mainScreenAnimationController: animationController,
-      ),
-    );
-  }
+  final int type;
+  final dynamic value;
 
-  Widget getMainListViewUI() {
-    //之前动画不显示是主动画没开始，导致初始进度为0
-    return ListView.builder(
-      controller: scrollController,
-      padding: EdgeInsets.only(
-        top: AppBar().preferredSize.height +
-            MediaQuery.of(context).padding.top +
-            24,
-        bottom: 62 + MediaQuery.of(context).padding.bottom,
-      ),
-      itemCount: listViews.length,
-      scrollDirection: Axis.vertical,
-      itemBuilder: (BuildContext context, int index) {
-        animationController.forward();
-        return listViews[index];
-      },
-    );
-  }
-
-  Widget getAppBarUI() {
-    int i = 0;
-    return Column(
-      children: <Widget>[
-        AnimatedBuilder(
-          animation: animationController,
-          builder: (BuildContext context, Widget child) {
-            return FadeTransition(
-              opacity: topBarAnimation,
-              child: Transform(
-                transform: Matrix4.translationValues(
-                    0.0, 30 * (1.0 - topBarAnimation.value), 0.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: AppTheme.white.withOpacity(topBarOpacity),
-                    borderRadius: const BorderRadius.only(
-                      bottomLeft: Radius.circular(32.0),
-                    ),
-                    boxShadow: <BoxShadow>[
-                      BoxShadow(
-                          color: AppTheme.grey.withOpacity(0.4 * topBarOpacity),
-                          offset: const Offset(1.1, 1.1),
-                          blurRadius: 10.0),
-                    ],
-                  ),
-                  child: Column(
-                    children: <Widget>[
-                      SizedBox(
-                        height: MediaQuery.of(context).padding.top,
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(
-                            left: 16,
-                            right: 16,
-                            top: 16 - 8.0 * topBarOpacity,
-                            bottom: 12 - 8.0 * topBarOpacity),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Expanded(
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Text(
-                                  '我的一天',
-                                  textAlign: TextAlign.left,
-                                  style: TextStyle(
-                                    fontFamily: AppTheme.fontName,
-                                    fontWeight: FontWeight.w700,
-                                    fontSize: 22 + 6 - 6 * topBarOpacity,
-                                    letterSpacing: 1.2,
-                                    color: AppTheme.darkerText,
-                                  ),
-                                ),
-                              ),
-                            ),
-                            SizedBox(
-                              height: 38,
-                              width: 38,
-                              child: GestureDetector(
-                                onTap: () {
-                                  i++;
-                                  print('add habit');
-                                  BlocProvider.of<HabitsBloc>(context)
-                                      .add(HabitsAdd(Habit.createHabit('$i')));
-                                  animationController.forward();
-                                },
-                                child: Center(
-                                  child: Icon(
-                                    Icons.add_alarm,
-                                    color: AppTheme.grey,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            );
-          },
-        )
-      ],
-    );
-  }
+  const ListData({this.type, this.value});
 }
