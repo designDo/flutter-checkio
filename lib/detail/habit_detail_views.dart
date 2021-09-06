52f21c2ce28814b1ae1c1c58c1f7481507f2c2c2
tangya <tangmaking@163.com>
2021-09-06 15:18:01 +0800

    most streaks view use chart

--- lib/detail/habit_detail_views.dart
+++ lib/detail/habit_detail_views.dart
@@ -1,3 +1,4 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:timefly/detail/detail_calender_view.dart';
@@ -6,6 +7,7 @@ import 'package:timefly/models/habit.dart';
 import 'package:timefly/models/habit_peroid.dart';
 import 'package:timefly/utils/date_util.dart';
 import 'package:timefly/utils/habit_util.dart';
+import 'package:timefly/utils/hex_color.dart';
 import 'package:timefly/utils/pair.dart';
 import 'package:timefly/widget/circle_progress_bar.dart';
 
@@ -792,7 +794,7 @@ class HabitCheckInfoView extends StatelessWidget {
   }
 }
 
-class HabitStreakInfoView extends StatelessWidget {
+class HabitStreakInfoView extends StatefulWidget {
   final Habit habit;
   final AnimationController animationController;
 
@@ -800,9 +802,18 @@ class HabitStreakInfoView extends StatelessWidget {
       : super(key: key);
 
   @override
+  State<StatefulWidget> createState() {
+    return HabitStreakInfoViewState();
+  }
+}
+
+class HabitStreakInfoViewState extends State<HabitStreakInfoView> {
+  int touchedIndex;
+
+  @override
   Widget build(BuildContext context) {
     Map<String, List<HabitRecord>> records =
-        HabitUtil.combinationRecords(habit.records);
+        HabitUtil.combinationRecords(widget.habit.records);
     Map<String, int> streaks = HabitUtil.getHabitStreaks(records);
     int maxCount = 0;
     if (streaks.length > 0) {
@@ -811,11 +822,11 @@ class HabitStreakInfoView extends StatelessWidget {
     return SlideTransition(
         position: Tween<Offset>(begin: Offset(0, 0.3), end: Offset.zero)
             .animate(CurvedAnimation(
-                parent: animationController,
+                parent: widget.animationController,
                 curve: Interval(0.7, 1, curve: Curves.ease))),
         child: FadeTransition(
           opacity: Tween<double>(begin: 0, end: 1).animate(CurvedAnimation(
-              parent: animationController,
+              parent: widget.animationController,
               curve: Interval(0.7, 1, curve: Curves.ease))),
           child: Container(
             margin: EdgeInsets.only(top: 16, right: 16, left: 16),
@@ -845,18 +856,12 @@ class HabitStreakInfoView extends StatelessWidget {
                     )
                   ],
                 ),
-                Text(
-                  '历史连续',
-                  style: AppTheme.appTheme
-                      .headline1(fontWeight: FontWeight.normal, fontSize: 14),
-                ),
                 streaks.length == 0
                     ? SizedBox()
-                    : Column(
-                        children: streaks.keys
-                            .take(5)
-                            .map((e) => _checkInfo(e, streaks[e], maxCount))
-                            .toList(),
+                    : Container(
+                        margin: EdgeInsets.only(top: 16, bottom: 16),
+                        height: 200,
+                        child: BarChart(barData(streaks, maxCount.toDouble())),
                       ),
               ],
             ),
@@ -864,66 +869,91 @@ class HabitStreakInfoView extends StatelessWidget {
         ));
   }
 
-  Widget _checkInfo(String time, int count, int maxCount) {
-    List<String> str = time.split(',');
-    return Row(
-      children: [
-        Container(
-          width: 140,
-          child: Stack(
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                    color: AppTheme.appTheme.containerBackgroundColor(),
-                    shape: BoxShape.rectangle,
-                    borderRadius: BorderRadius.all(Radius.circular(10))),
-                height: 10,
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    flex: count,
-                    child: Container(
-                      decoration: BoxDecoration(
-                          color: Color(habit.mainColor).withOpacity(0.6),
-                          shape: BoxShape.rectangle,
-                          borderRadius: BorderRadius.all(Radius.circular(10))),
-                      height: 10,
-                    ),
-                  ),
-                  Expanded(
-                    flex: maxCount - count,
-                    child: SizedBox(),
-                  )
-                ],
-              )
-            ],
+  BarChartData barData(Map<String, int> streaks, double maxY) {
+    List<String> keys = streaks.keys.toList();
+    return BarChartData(
+      maxY: maxY * 1.5,
+      barTouchData: BarTouchData(
+        touchTooltipData: BarTouchTooltipData(
+            fitInsideHorizontally: true,
+            fitInsideVertically: true,
+            tooltipRoundedRadius: 16,
+            tooltipPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+            tooltipBgColor: Color(widget.habit.mainColor).withOpacity(0.66),
+            getTooltipItem: (group, groupIndex, rod, rodIndex) {
+              return BarTooltipItem(
+                  '${getTime(keys[groupIndex])}\n${rod.y.toInt()}',
+                  AppTheme.appTheme.numHeadline1(
+                    textColor: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ));
+            }),
+        touchCallback: (event, barTouchResponse) {
+          setState(() {
+            if (barTouchResponse?.spot != null) {
+              touchedIndex = barTouchResponse.spot.touchedBarGroupIndex;
+            } else {
+              touchedIndex = -1;
+            }
+          });
+        },
+      ),
+      titlesData: FlTitlesData(
+          show: true,
+          leftTitles: SideTitles(
+              showTitles: true,
+              getTextStyles: (context, value) {
+                return AppTheme.appTheme.numHeadline1(
+                  textColor: AppTheme.appTheme.normalColor(),
+                  fontSize: 13,
+                  fontWeight: FontWeight.bold,
+                );
+              }),
+          topTitles: SideTitles(showTitles: false),
+          bottomTitles: SideTitles(showTitles: false),
+          rightTitles: SideTitles(showTitles: false)),
+      borderData: FlBorderData(show: false),
+      gridData: FlGridData(show: false),
+      barGroups: showingGroups(streaks, maxY),
+    );
+  }
+
+  List<BarChartGroupData> showingGroups(Map<String, int> streaks, double maxY) {
+    List<int> valuse = streaks.values.toList();
+    return List.generate(valuse.length, (i) {
+      return makeGroupData(i, valuse[i].toDouble(),
+          isTouched: i == touchedIndex);
+    });
+  }
+
+  BarChartGroupData makeGroupData(
+    int x,
+    double currentY, {
+    bool isTouched = false,
+    double width = 12,
+    List<int> showTooltips = const [],
+  }) {
+    return BarChartGroupData(
+      barsSpace: 6,
+      x: x,
+      barRods: [
+        BarChartRodData(
+          y: currentY,
+          colors: [Color(widget.habit.mainColor)],
+          width: width,
+          backDrawRodData: BackgroundBarChartRodData(
+            show: false,
           ),
-        ),
-        Expanded(
-          child: SizedBox(),
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Text(
-              '$count',
-              style: AppTheme.appTheme
-                  .numHeadline1(fontWeight: FontWeight.bold, fontSize: 22),
-            ),
-            SizedBox(
-              width: 10,
-            ),
-            Text(
-              '${str[0].replaceAll('-', '.')} - ${str[1].substring(str[1].indexOf('-') + 1).replaceAll('-', '.')}',
-              maxLines: 2,
-              style: AppTheme.appTheme
-                  .numHeadline1(fontSize: 16, fontWeight: FontWeight.w300),
-            )
-          ],
         )
       ],
+      showingTooltipIndicators: showTooltips,
     );
+  }
+
+  String getTime(String time) {
+    List<String> str = time.split(',');
+    return ' ${str[0].replaceAll('-', '.')} - ${str[1].substring(str[1].indexOf('-') + 1).replaceAll('-', '.')}';
   }
 }
 
