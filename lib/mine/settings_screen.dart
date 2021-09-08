ff76f3e5eb52f4ff1cf83109ed89dc13f537a6fd
tangya <tangmaking@163.com>
2021-09-08 16:58:46 +0800

    user phone show

--- lib/mine/settings_screen.dart
+++ lib/mine/settings_screen.dart
@@ -23,59 +23,61 @@ class _SettingsScreenState extends State<SettingsScreen> {
       builder: (context, state) {
         SystemUtil.changeStateBarMode(
             AppTheme.appTheme.isDark() ? Brightness.light : Brightness.dark);
-        return Container(
-          color: AppTheme.appTheme.containerBackgroundColor(),
-          child: Stack(
-            children: [
-              ListView(physics: ClampingScrollPhysics(), children: [
-                Container(
-                  margin: EdgeInsets.only(
-                      top: MediaQuery.of(context).padding.top, right: 24),
-                  alignment: Alignment.topRight,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.all(Radius.circular(16)),
-                    child: Image.network(
-                      'https://c-ssl.duitang.com/uploads/item/201903/06/20190306091021_kzhxr.jpg',
-                      width: 130,
-                      height: 130,
-                      fit: BoxFit.cover,
+        return Scaffold(
+          body: Container(
+            color: AppTheme.appTheme.containerBackgroundColor(),
+            child: Stack(
+              children: [
+                ListView(physics: ClampingScrollPhysics(), children: [
+                  Container(
+                    margin: EdgeInsets.only(
+                        top: MediaQuery.of(context).padding.top, right: 24),
+                    alignment: Alignment.topRight,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.all(Radius.circular(16)),
+                      child: Image.network(
+                        'https://c-ssl.duitang.com/uploads/item/201903/06/20190306091021_kzhxr.jpg',
+                        width: 130,
+                        height: 130,
+                        fit: BoxFit.cover,
+                      ),
                     ),
                   ),
-                ),
-                ChangeUserInfoView(),
-                SizedBox(
-                  height: 100,
-                )
-              ]),
-              GestureDetector(
-                onTap: () {
-                  Navigator.of(context).pop();
-                },
-                child: Container(
-                  alignment: Alignment.centerLeft,
-                  margin: EdgeInsets.only(
-                      top: MediaQuery.of(context).padding.top + 26),
-                  height: 45,
+                  ChangeUserInfoView(),
+                  SizedBox(
+                    height: 100,
+                  )
+                ]),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).pop();
+                  },
                   child: Container(
-                    alignment: Alignment.center,
-                    width: 90,
-                    decoration: BoxDecoration(
-                        shape: BoxShape.rectangle,
-                        borderRadius: BorderRadius.only(
-                            topRight: Radius.circular(26),
-                            bottomRight: Radius.circular(26)),
-                        color: AppTheme.appTheme.cardBackgroundColor(),
-                        boxShadow: AppTheme.appTheme.containerBoxShadow()),
-                    child: SvgPicture.asset(
-                      'assets/images/fanhui.svg',
-                      width: 25,
-                      height: 25,
-                      color: AppTheme.appTheme.normalColor().withOpacity(0.8),
+                    alignment: Alignment.centerLeft,
+                    margin: EdgeInsets.only(
+                        top: MediaQuery.of(context).padding.top + 26),
+                    height: 45,
+                    child: Container(
+                      alignment: Alignment.center,
+                      width: 90,
+                      decoration: BoxDecoration(
+                          shape: BoxShape.rectangle,
+                          borderRadius: BorderRadius.only(
+                              topRight: Radius.circular(26),
+                              bottomRight: Radius.circular(26)),
+                          color: AppTheme.appTheme.cardBackgroundColor(),
+                          boxShadow: AppTheme.appTheme.containerBoxShadow()),
+                      child: SvgPicture.asset(
+                        'assets/images/fanhui.svg',
+                        width: 25,
+                        height: 25,
+                        color: AppTheme.appTheme.normalColor().withOpacity(0.8),
+                      ),
                     ),
                   ),
-                ),
-              )
-            ],
+                )
+              ],
+            ),
           ),
         );
       },
@@ -103,7 +105,7 @@ class _ChangeUserInfoViewState extends State<ChangeUserInfoView> {
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.only(top: 32, left: 22, right: 22),
-      padding: EdgeInsets.only(top: 16, bottom: 16),
+      padding: EdgeInsets.only(top: 16, bottom: 24),
       decoration: BoxDecoration(
           shape: BoxShape.rectangle,
           borderRadius: BorderRadius.all(Radius.circular(15)),
@@ -118,9 +120,9 @@ class _ChangeUserInfoViewState extends State<ChangeUserInfoView> {
             initValue: userName,
             hintText: '名字',
             hintTextStyle: AppTheme.appTheme
-                .hint(fontWeight: FontWeight.normal, fontSize: 16),
+                .hint(fontWeight: FontWeight.bold, fontSize: 18),
             textStyle: AppTheme.appTheme
-                .headline1(fontWeight: FontWeight.normal, fontSize: 16),
+                .headline1(fontWeight: FontWeight.bold, fontSize: 18),
             containerDecoration: BoxDecoration(
                 shape: BoxShape.rectangle,
                 borderRadius: BorderRadius.all(Radius.circular(15)),
@@ -150,6 +152,23 @@ class _ChangeUserInfoViewState extends State<ChangeUserInfoView> {
                 print(e);
               }
             },
+          ),
+          Container(
+            padding: EdgeInsets.only(left: 16),
+            alignment: Alignment.centerLeft,
+            margin: EdgeInsets.only(left: 32, right: 32, top: 16),
+            decoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.all(Radius.circular(15)),
+                color: AppTheme.appTheme.containerBackgroundColor()),
+            height: 65,
+            child: Text(
+              SessionUtils.sharedInstance().currentUser.phone,
+              style: AppTheme.appTheme.numHeadline1(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 23,
+                  textColor: AppTheme.appTheme.normalColor().withOpacity(0.5)),
+            ),
           )
         ],
       ),
